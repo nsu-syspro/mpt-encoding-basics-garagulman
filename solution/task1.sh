3b47d1e1622d7b2643e7b501dac650c567a1676e
garagulman <akgaragulova@gmail.com>
2025-09-03 16:30:52 +0700

    task1234

---
+++ solution/task1.sh
@@ -0,0 +1,3 @@
+iconv -f -t utf-8 task2.sh
+iconv -f IBM866 -t UTF-8 task3.sh
+iconv -f ISO_8859-5 -t UTF-8 task4.sh
--- solution/task4.sh
+++ solution/task4.sh
@@ -3,3 +3,4 @@
 # Вывести дату в формате:
 #   Freitag, 13. Oktober 2023
 date '+???' -d "$@"
+echo "$(LC_TIME=de_DE date +'%A'), $(date +'%d'). $(LC_TIME=en_US date +'%B') $(date +'%Y')"
