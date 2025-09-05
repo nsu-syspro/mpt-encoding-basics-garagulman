a0d77da5e654dccefc948ff8748abe63d36c193e
garagulman <akgaragulova@gmail.com>
2025-09-05 15:24:21 +0700

    task4

--- solution/task4.sh
+++ solution/task4.sh
@@ -2,5 +2,5 @@
 
 # Вывести дату в формате:
 #   Freitag, 13. Oktober 2023
-date '+???' -d "$@"
-echo "$(LC_TIME=de_DE date +'%A'), $(date +'%d'). $(LC_TIME=en_US date +'%B') $(date +'%Y')"
+#date '+???' -d "$@"
+echo "$(LC_TIME=de_DE date +'%A' -d "$@"), $(date +'%d' -d "$@"). $(LC_TIME=en_US date +'%B' -d "$@") $(date +'%Y' -d "$@")"
