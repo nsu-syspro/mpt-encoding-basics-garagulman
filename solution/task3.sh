e57a5d4a76e731c460c96b0fe1acb0e57e69043f
garagulman <akgaragulova@gmail.com>
2025-09-06 15:27:51 +0700

    task3

--- solution/task3.sh
+++ solution/task3.sh
@@ -2,6 +2,4 @@
 
 # Вывести дату в формате:
 #   13 октября 2023 года, Пятница
-date '+d-%B:%Y:%A' -d "$@"
-#LC_ALL=C LANG=ru_RU.utf8
-#echo "$(date +'%d' -d "$@") $(LC_TIME=ru_RU.utf8 date +'%B' -d "$@") $(date +'%Y' -d "$@") года, $(LC_TIME=ru_RU.utf8 date +"%A" -d "$@")"
+date '+%d %B %Y года, %A' -d "$@"
