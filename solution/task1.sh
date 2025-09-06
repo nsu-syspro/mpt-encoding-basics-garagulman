8b1135557eac6a84337a81660861b273e99563c2
garagulman <akgaragulova@gmail.com>
2025-09-06 14:41:46 +0700

    task1

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-iconv -f KOI8-R -t UTF-8 solution/task2.sh > solution/tmp.sh | cp solution/tmp.sh solution/task2.sh
+iconv -f KOI8-R -t UTF-8 task2.sh > tmp.sh | cp tmp.sh task2.sh
 iconv -f IBM866 -t UTF-8 task3.sh > tmp.sh | cp tmp.sh task3.sh
 iconv -f ISO_8859-5 -t UTF-8 task4.sh > tmp.sh | cp tmp.sh task4.sh
